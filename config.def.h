72bb86a53033221409982dcc5a122d6c294fded6
DAF <>
2022-03-10 17:15:15 -0500

    Add grid patch

--- config.def.h
+++ config.def.h
@@ -19,8 +19,9 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 0;
+static unsigned int columns    = 0;
 static unsigned int maxhist    = 64;
 static int histnodup           = 1;	/* if 0, record repeated histories */
 
